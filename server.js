c0ea02cc77d478d5ce167294083b32a9f517fab8
unknown <shayan.ali.mansehra@gmail.com.com>
2026-06-28 14:47:03 +0500

    backend completed

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import statsRoutes from './src/Routes/statsroutes.js';
+import traceabilityRoutes from './src/Routes/traceabilityroutes.js';
+import leadroutes from './src/Routes/leadroutes.js';
+import productsroutes from './src/Routes/productsroutes.js';
 import authroutes from './src/Routes/Auth.js';
 import connectDB from './src/Config/db.js'; // Note the explicit .js extension required in ES Modules
 
@@ -19,6 +23,10 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/auth', authroutes);
+app.use('/api/products', productsroutes);
+app.use('/api/leads', leadroutes);
+app.use('/api/traceability', traceabilityRoutes);
+app.use('/api/stats', statsRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
